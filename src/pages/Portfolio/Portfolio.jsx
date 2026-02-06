8240f3d4b47eb11fbd6b079c591f5569ddbb7f15
Kamil Gadzicki <kam.gadzicki@gmail.com>
2026-02-06 17:43:29 +0100

    Portfolio: improve layout, grid responsiveness and hover effects

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -5,45 +5,46 @@ import nestingImage from "../../assets/nesting.jpg";
 
 function Portfolio() {
   return (
-    <>
+    <div className="portfolioCss">
       <div className="portfolioImage"></div>
       <h1 className="category">Category</h1>
+
       <div className="categoryContainer">
         <section className="composites">
           <img src={compositeImage} className="imageComposites"></img>
-          <h3>Composites</h3>
+          <h3 className="title">Composites</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Praesentium debitis, temporibus sed dolorum tempore alias numquam
-            doloribus porro architecto unde, perspiciatis qui nobis quas
-            suscipit vel repudiandae voluptatibus? Aspernatur, est.
+            Practical projects focused on composite materials. Solving real
+            technological problems from production and testing processes.
           </p>
           <button className="btn btn-primary">View projects</button>
         </section>
         <section className="nesting">
           <img src={nestingImage} className="imageNesting"></img>
-          <h3>Nesting</h3>
+          <h3 className="title">Nesting</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Praesentium debitis, temporibus sed dolorum tempore alias numquam
-            doloribus porro architecto unde, perspiciatis qui nobis quas
-            suscipit vel repudiandae voluptatibus? Aspernatur, est.
+            Optimization of part placement on material sheets. Reducing waste
+            and improving production efficiency.
           </p>
           <button className="btn btn-primary">View projects</button>
         </section>
         <section className="honeycomb">
           <img src={honeybombImage} className="imageHoneycomb"></img>
-          <h3>Honeycomb</h3>
+          <h3 className="title">Honeycomb</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Praesentium debitis, temporibus sed dolorum tempore alias numquam
-            doloribus porro architecto unde, perspiciatis qui nobis quas
-            suscipit vel repudiandae voluptatibus? Aspernatur, est.
+            Work with honeycomb-based structures. Analyzing and solving quality
+            issues in manufacturing.
           </p>
           <button className="btn btn-primary">View projects</button>
         </section>
       </div>
-    </>
+      <div className="categoryButton">
+        <button className="btn btn-primary">All</button>
+        <button className="btn btn-primary">Composites</button>
+        <button className="btn btn-primary">Nesting</button>
+        <button className="btn btn-primary">Heneycomb</button>
+      </div>
+    </div>
   );
 }
 
